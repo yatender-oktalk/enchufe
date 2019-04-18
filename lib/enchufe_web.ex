2b7111b6b580049e5f5cf8e076123c5a741af237
Yatender <yatender@getvokal.com>
2019-04-18 10:43:05 +0530

    formatting

--- lib/enchufe_web.ex
+++ lib/enchufe_web.ex
@@ -28,8 +28,9 @@ defmodule EnchufeWeb do
 
   def view do
     quote do
-      use Phoenix.View, root: "lib/enchufe_web/templates",
-                        namespace: EnchufeWeb
+      use Phoenix.View,
+        root: "lib/enchufe_web/templates",
+        namespace: EnchufeWeb
 
       # Import convenience functions from controllers
       import Phoenix.Controller, only: [get_flash: 2, view_module: 1]
